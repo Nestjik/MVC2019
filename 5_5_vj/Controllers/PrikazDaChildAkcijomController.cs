dd16c6e1aa0ac6022843030afe808b0e4a01ac3d
Nestjit <Tesl@physicist.net>
2019-04-15 18:23:06 +0200

    vj

---
+++ 5_5_vj/Controllers/PrikazDaChildAkcijomController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _5_5_vj.Models;
+
+namespace _5_5_vj.Controllers
+{
+    public class PrikazDaChildAkcijomController : Controller
+    {
+        public ViewResult ObradiListu()
+        {
+            List<Artikl> lArt = new List<Artikl>
+            {
+                new Artikl(){Kategorija="voce",Naziv="Banana",Cijena=10.9m,Kolicina=10},
+                new Artikl(){Kategorija="povrce",Naziv="Mrkva",Cijena=5.9m,Kolicina=2},
+                new Artikl(){Kategorija="meso",Naziv="Kobaja",Cijena=30.9m,Kolicina=8}
+            };
+            return View(lArt);
+        }
+    }
+}
---
+++ View/Controllers/LayoutSectionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class LayoutSectionController : Controller
+    {
+        public ViewResult Stranica1()
+        {
+            return View();
+        }
+
+        public ViewResult Stranica2()
+        {
+            return View();
+        }
+    }
+}
